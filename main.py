137b5cab91f102c9f3b7aa12252d3960d87f464e
SlimyRedstone <49628477+SlimyRedstone@users.noreply.github.com>
2023-09-04 01:39:09 +0200

    Add files via upload
    Fixed the method to get the time of the movie

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import json
 from datetime import datetime
 import subprocess as subp
+import time
 
 DEBUG = False
 LINE_CLEAR = '\x1b[2K'
@@ -27,22 +28,23 @@ def numberToDateTime(_t: int | float) -> str:
     return datetime.fromtimestamp(_t).strftime("%d/%m/%Y at %H:%M:%S")
 
 def getDuration(_input: dict = {}) -> str:
-    _d = _input.copy()
-    global _ftime
-    _ftime = ""
-    if "duration" in _d:
-        _temp = int(float(_d["duration"]))
-        _ftime = numberToTime(_temp)
-        # print("# %s"%_ftime)
-    elif "duration_ts" in _d:
-        _temp = int(_d["duration_ts"]) / 1000
-        _ftime = numberToTime(_temp)
-    if "tags" in _d and len(_ftime) < 1:
-        # _d = _d["tags"]
-        for _i, _v in enumerate(_d["tags"]):
-            if _v.lower().startswith("duration"):
-                _ftime = _d["tags"][_v][0:8]
-    return _ftime
+    copy_stream = _input.copy()
+    global formatted_time
+    formatted_time = ""
+    # print('\nSearching for duration...\n%s' % json.dumps(_input, indent=2))
+    if "duration" in copy_stream:
+        _temp = int(float(copy_stream["duration"]))
+        return {'error': False, 'result': numberToTime(_temp)}
+
+    elif "duration_ts" in copy_stream:
+        _temp = int(copy_stream["duration_ts"]) / 1000
+        return {'error': False, 'result': numberToTime(_temp)}
+
+    elif "tags" in copy_stream:
+        for _i, key in enumerate(copy_stream["tags"]):
+            if str(key).lower().startswith("duration"):
+                return {'error': False, 'result': copy_stream["tags"][key][0:8]}
+    return {'error': True, 'result': ''}
 
 def clearJSON(_json) -> dict:
     return json.loads(json.dumps(str(_json)))
@@ -82,7 +84,15 @@ def splitPath(path:pathlib.Path)->list:
 def getParentFolder(path):
     return os.path.dirname(path).split('\\')[-1]
 
+
+def debugJSON(name:str,data:dict={}):
+    path = os.path.join('debug',name)
+    with open(path,'w') as f:
+        f.write( json.dumps(data, indent=2) )
+
 def getMovieInfo(path:pathlib.Path)->dict:
+    global movie_data
+    global movie_duration
     movie_data = {
         'name': '',
         'ext': '',
@@ -92,14 +102,16 @@ def getMovieInfo(path:pathlib.Path)->dict:
         'audio': [],
         'subtitles': [],
         'video': {
-            'h': -1,
-            'w': -1,
+            'h': 0,
+            'w': 0,
             't': -1,
         },
         'error': False,
         'output': {}
     }
     
+    movie_duration = { 'error': True, 'result': ''}
+
     ffmpeg_cmd = [
         "ffprobe",
         "-hide_banner",
@@ -120,57 +132,61 @@ def getMovieInfo(path:pathlib.Path)->dict:
     ffmpeg_json = json.loads(cmd_output)
 
     if 'streams' in ffmpeg_json:
-        for index, stream in enumerate(ffmpeg_json['streams']):
+        if DEBUG:
+            print('Streams: %d' % len(ffmpeg_json['streams']))
+            debugJSON('streams.json',ffmpeg_json)
+            
+            for stream in ffmpeg_json['streams']:
+                print(json.dumps(stream))
+
+        for stream in ffmpeg_json['streams']:
             if 'codec_type' in stream:
                 codec_type = stream['codec_type']
+                del(stream['disposition'])
                 
-                match (codec_type):
-                    case 'audio': 
-                        if 'tags' in stream:
-                            if movie_data['video']['t'] != -1:
-                                movie_data['video']['t'] = getDuration(stream['tags'])
-                            
-                            if 'language' in stream['tags']:
-                                lang = stream['tags']['language'][0:2]
-                                if not lang in movie_data['audio']:
-                                    if len(lang):
-                                        movie_data['audio'].append(lang)
-                        break
+                if movie_duration['error']:
+                    movie_duration = getDuration(stream)
+                    movie_data['video']['t'] = movie_duration['result']
 
-                    case 'video':
-                        if 'profile' in stream:
-                            if stream['codec_name'] in ['hevc','h264','mpeg4']:
-                                if stream['profile'] in ['High','Main','Main 10','Advanced Simple Profile']:
-                                    if movie_data['video']['t'] == -1:
-                                        movie_data["video"]['t'] = getDuration(stream)
-                                
-                                if movie_data['video']['h'] == -1:
-                                    movie_data['video']['h'] = stream['height']
-                                if movie_data['video']['w'] == -1:
-                                    movie_data['video']['w'] = stream['width']
-                        break
+                if codec_type == 'audio':
+                    if 'tags' in stream:
+                        if 'language' in stream['tags']:
+                            lang = stream['tags']['language'][0:2]
+                            if not lang in movie_data['audio']:
+                                if len(lang):
+                                    movie_data['audio'].append(lang)
+                        if DEBUG:
+                            debugJSON('audio.json',stream)
 
-                    case 'subtitle':
-                        if movie_data['video']['t'] != -1:
-                            movie_data['video']['t'] = getDuration(stream)
+                elif codec_type == 'video':
+                    if 'profile' in stream:
+                        if stream['codec_name'] in ['hevc','h264','mpeg4']:
+                            # if stream['profile'] in ['High','Main','Main 10','Advanced Simple Profile']:
+                            if movie_data['video']['h'] == 0:
+                                movie_data['video']['h'] = stream['height']
+                            if movie_data['video']['w'] == 0:
+                                movie_data['video']['w'] = stream['width']
+                        if DEBUG:
+                            debugJSON('video.json',stream)
 
-                        if 'tags' in stream:
-                            if 'language' in stream['tags']  and  'title' in stream['tags']:
-                                lang_sub = stream['tags']['language'][0:2]
-                                _lang = (
-                                    "".join(["+", stream["tags"]["title"].lower()])
-                                    if "title" in stream["tags"]
-                                    else ""
-                                )
-                                formatted_sub = ''.join([lang_sub, _lang])
+                elif codec_type == 'subtitle':
+                    if 'tags' in stream:
+                        if 'language' in stream['tags']:
+                            lang_sub = stream['tags']['language'][0:2]
 
-                                if not formatted_sub in movie_data['subtitles']:
-                                    if len(stream['tags']['language']):
-                                        movie_data['subtitles'].append(formatted_sub)
-                        break
+                            if not lang_sub in movie_data['subtitles']:
+                                if len(lang_sub):
+                                    movie_data['subtitles'].append(lang_sub)
+                            if DEBUG:
+                                debugJSON('subtitle.json',stream)
+
 
     movie_data['error'] = False if 'streams' in ffmpeg_json else True
     movie_data['output'] = {} if 'streams' in ffmpeg_json else cmd_output
+
+    if DEBUG:
+        debugJSON('data.json',movie_data)
+
     return movie_data
 
 if __name__ == '__main__':
@@ -182,6 +198,7 @@ if __name__ == '__main__':
         parentFolder = getParentFolder(movie)
 
         if not parentFolder in BLACKLISTED_FOLDERS:
+        # if not parentFolder in BLACKLISTED_FOLDERS and file_name == 'The Menu':
             movie_data = getMovieInfo(movie)
             movie_data['index'] = index
 
@@ -190,9 +207,13 @@ if __name__ == '__main__':
 
             # movie_db['list'].append( json.loads(movie_data))
             movie_db['list'].append( movie_data )
+            # if DEBUG:
+            #     time.sleep(5)
     print()
         
-    with open('ffmpeg.json','w') as file:
+    movie_db['list'].sort(key=lambda v: v['size'], reverse=True)
+
+    with open(DB_FILE_NAME,'w') as file:
         file.write( json.dumps(movie_db,indent= 2) )
 
     print( 'Length: %d' % len(files))
